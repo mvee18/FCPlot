959508789ccc7377a5b44711b65d7aa825f8356b
michael <emvalenc@go.olemiss.edu>
2020-06-04 17:03:52 -0500

    Finished data importation and refactored slightly.

--- main.py
+++ main.py
@@ -2,15 +2,34 @@ import numpy as np
 import os
 
 # This function will return a numpy array from the fort files.
-
-
-def generatearray(file):
+def generate_array(file):
     path = os.path.join(file)
     return np.asarray(np.genfromtxt(path, skip_header=1))
 
+def generate_data(level):
+    fort15data = generate_array("fort.15")
+    fort30data = generate_array("fort.30")
+    fort40data = generate_array("fort.40")
 
-fort15data = generatearray("fort.15")
-fort30data = generatearray("fort.30")
-fort40data = generatearray("fort.40")
+    if level == 2:
+        return fort15data
+    elif level == 3:
+        return fort15data, fort30data
+    elif level == 4:
+        return fort15data, fort30data, fort40data
 
-print(fort15data[16][2])
+
+# Taylor Series Calculations.
+def taylor_series(level):
+    if level == 2:
+        data = generate_data(2)
+        print(data[0][16][2])
+    elif level == 3:
+        data = generate_data(3)
+        print(data[0][16][2])
+    elif level == 4:
+        data = generate_data(3)
+        print(data[0][16][2])
+    else:
+        raise Exception("Invalid Level. Must be 2,3,4.")
+
