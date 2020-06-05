202fa7b8c5a683d4f7dd3e4b855d3d4d57e44d66
michael <emvalenc@go.olemiss.edu>
2020-06-05 17:52:30 -0500

    fixed taylor series

--- main.py
+++ main.py
@@ -35,28 +35,41 @@ def taylor_series(level):
         raise Exception("Invalid Level. Must be 2,3,4.")
 
 
-f1 = 0.45961573569854314
-f2 = 0.40010514709525397
-f3 = -1.839080919045915
+# These are the z2 displacements.
+# f_constants = [0.45961573569854314, 0.40010514709525397, -1.839080919045915]
+
+
+# These are the z3 displacements.
+f_constants = [0.21817255812613368, -0.17041516191284814, -0.8302383821581545]
+
+f1 = abs(f_constants[0])
+f2 = abs(f_constants[1])
+f3 = abs(f_constants[2])
 
 def plot_from_tuples(data):
     y_val = [x[0] for x in data]
     x_val = [x[1] for x in data]
 
-    print(x_val)
     plt.plot(x_val, y_val)
     plt.plot(x_val, y_val, 'or')
+
+# It is interesting to note that if the below code is added, then the plot is a linear graph...
+#    plt.xscale('log')
+#    plt.yscale('log')
     plt.show()
 
-# F'(a)/1
+# Check that the signs of the values are correct. I don't anticipate that the signs need to change.
+
 def summation_of_terms(z):
     points = []
 
     for x in range(z):
         print(x)
-        points.append((0 + (f1/1)*x + (f2/2)*(x**2) + (f3/6)*(x**3), x))
+        points.append((0 + (f1/2)*(x**2) + (f2/6)*(x**3) + (f3/24)*(x**4), x))
 
     plot_from_tuples(points)
 
-summation_of_terms(8)
 
+summation_of_terms(10)
+
+# Still need to add curve fitting to get a function out.
