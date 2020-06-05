1abc24d399a91c1b344038a7650e8341414145c8
michael <emvalenc@go.olemiss.edu>
2020-06-05 15:59:25 -0500

    Changed Readme. Added plotting from basic Taylor Series.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 # This function will return a numpy array from the fort files.
 def generate_array(file):
@@ -19,7 +20,7 @@ def generate_data(level):
         return fort15data, fort30data, fort40data
 
 
-# Taylor Series Calculations.
+# Taylor Series Calculations. Find a way to find each point...?
 def taylor_series(level):
     if level == 2:
         data = generate_data(2)
@@ -32,4 +33,30 @@ def taylor_series(level):
         print(data[0][16][2])
     else:
         raise Exception("Invalid Level. Must be 2,3,4.")
+
+
+f1 = 0.45961573569854314
+f2 = 0.40010514709525397
+f3 = -1.839080919045915
+
+def plot_from_tuples(data):
+    y_val = [x[0] for x in data]
+    x_val = [x[1] for x in data]
+
+    print(x_val)
+    plt.plot(x_val, y_val)
+    plt.plot(x_val, y_val, 'or')
+    plt.show()
+
+# F'(a)/1
+def summation_of_terms(z):
+    points = []
+
+    for x in range(z):
+        print(x)
+        points.append((0 + (f1/1)*x + (f2/2)*(x**2) + (f3/6)*(x**3), x))
+
+    plot_from_tuples(points)
+
+summation_of_terms(8)
 
