112af2eab80858198a0f2d410e3cbd1d4dd9d8b3
michael <emvalenc@go.olemiss.edu>
2020-07-09 02:41:46 -0500

    cleaned up unnecessary commented code

--- flatten.py
+++ flatten.py
@@ -10,50 +10,6 @@ def generate_array(file):
     path = os.path.join(file)
     return np.asarray(np.genfromtxt(path, skip_header=1))
 
-"""
-def flatten_array(array):
-    array = array.flatten()
-    return array
-
-
-# Okay, this seems to work well. Let's try the other arrays and then put it all together.
-def second_array_matching():
-    fort15 = generate_array("fort_files/fort.15")
-    fort15shape = fort15.shape
-
-    second_coords = second_coordinates("fort_files/fort.15")
-    stacked = np.reshape(second_coords, (fort15shape[0], fort15shape[1], 4))
-
-#   for rows in range(fort15shape[0]):
-#       for cols in range(fort15shape[1]):
-#           print(fort15[rows][cols], stacked[rows][cols])
-
-
-def third_array_matching():
-    fort30 = generate_array("fort_files/fort.30")
-    fort30shape = fort30.shape
-
-    third_coords = third_geometry()
-    reshape_third = np.reshape(third_coords, (fort30shape[0], fort30shape[1], 3, 2))
-    reshape_third_shape = reshape_third.shape
-
-#   for rows in range(reshape_third_shape[0]):
-#       for cols in range(reshape_third_shape[1]):
-#           print(reshape_third[rows][cols], fort30[rows][cols])
-
-
-def fourth_array_matching():
-    fort40 = generate_array("fort_files/fort.40")
-    fort40shape = fort40.shape
-
-    fourth_coords = fourth_geometry()
-    reshape_fourth = np.reshape(fourth_coords, (165, 3, 4, 2))
-
-#   for rows in range(fort40shape[0]):
-#       for cols in range(fort40shape[1]):
-#           print(reshape_fourth[rows][cols], fort40[rows][cols])
-"""
-
 # Put it all together in one beautiful function. DONE!
 def array_matching(filename):
     fort_file = os.path.join("fort_files/" + filename)
@@ -135,38 +91,3 @@ iterate_arrays()
 function_list = np.asarray(function_list)
 print(function_list)
 
-"""
-This is why the arrays do not line up. The reciprocal points are used.
-array[a1, c1, a2, c2] = energy
-array[a2, c2, a1, c1] = energy
-
-Reset_List = []
-def iterate_array(array1):
-    array1 = flatten_array(array1)
-    size1 = array1.shape  # (3, 27)
-    print(size1)
-
-    global second_iter, Reset_List
-
-    for rows in range(size1[0]):
-        try:
-            f1 = (array1[rows])
-            c2 = next(second_iter)
-            Reset_List.append(list(c2))
-            print(rows, f1, c2)
-            break
-
-        except StopIteration:
-            Reset_List = np.asarray(Reset_List)
-            # print(Reset_List)
-            # print("You must construct additional pylons.")
-            second_iter = iter(second_coords)
-            f1 = (array1[rows])
-            c2 = next(second_iter)
-            print(f1, c2)
-
-            Reset_List = []
-            continue
-
-iterate_array(fort15)
-"""
