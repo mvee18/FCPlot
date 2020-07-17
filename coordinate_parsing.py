7796955484942e80a9e6d1a719ffb37af13cb781
michael <emvalenc@go.olemiss.edu>
2020-07-17 17:44:40 -0500

    need to expand coordinate parsing, before applying fixes

--- coordinate_parsing.py
+++ coordinate_parsing.py
@@ -3,13 +3,14 @@ from generatecoordinates import read_first_line
 
 # We are looking for the format [x1, y1, z1 ... xn, yn, zn] for each energy point.
 
-def parse_coordinates(coordinate):
-    num_atoms = int(read_first_line("fort_files/fort.15")[0])
+num_atoms = int(read_first_line("fort_files/fort.15")[0])
+
+def parse_coordinates(coordinate, sign):  # 1 means positive, -1 means negative.
     # We can use a numpy array with zero to represent the string of coordinates without any displacement.
     xyz = np.zeros((num_atoms, 3))
 
-    if len(coordinate) == 4:
-        coordinate = np.asarray(coordinate)
+    coordinate = np.asarray(coordinate)
+    if coordinate.size == 4:
         coordinate = np.reshape(coordinate, (2, 2))
 
         for entry in coordinate:
@@ -20,15 +21,17 @@ def parse_coordinates(coordinate):
         for entry in coordinate:
             xyz[entry[0]][entry[1]] += 1
 
-    return xyz.flatten()
+    if sign == 1:
+        return xyz.flatten()
+    elif sign == -1:
+        return np.negative(xyz.flatten())
 
 
-test1 = [0, 0, 0, 0]
-test2 = [1, 1, 1, 1]
+test1 = [0, 0, 0, 0]  # Outputs [2, 0, 0, 0, 0, 0, 0, 0, 0]
+test2 = [1, 1, 1, 1]  # Outputs [0, 0, 0, 0, 2, 0, 0, 0, 0]
 test3 = [2, 1, 2, 0]
 
 test4 = [[0, 0], [0, 0], [0, 0]]
 test5 = [[2, 0], [1, 0], [0, 0]]
 
-xyz_new = parse_coordinates(test3)
-print(xyz_new)
+print(parse_coordinates(test5, 1))
